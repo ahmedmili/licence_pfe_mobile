0799008c1fb6656f9677ba8ca55e602aaa325544
mili ahmed <ahmedmili76@gmail.com>
2023-04-02 05:14:50 +0100

    [Feat] fetch and display partner boxs
    get partner boxs from ther server and display them

--- lib/Screnns/box_screen.dart
+++ lib/Screnns/box_screen.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import '../Services/partners_service.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
+
+import '../model/boxs.dart';
+import '../widget/boxCard.dart';
 
 class BoxScreen extends StatefulWidget {
   const BoxScreen({Key? key}) : super(key: key);
@@ -17,32 +18,59 @@ class _BoxScreenState extends State<BoxScreen> {
   void initState() {
     super.initState();
     readToken();
+    // getPartnerBoxs();
   }
 
-  Future<void> getPartnerBoxs() async {
-    try {
-      http.Response response = await PartnersService.getBoxs(token);
-      List<dynamic> responseList = jsonDecode(response.body);
-      for (var responseMap in responseList) {
-        // print(responseMap);
-      }
-    } catch (e) {
-      print('Error: $e');
-    }
-  }
+  // Future<void> getPartnerBoxs() async {
+  //   try {
+  //     List<Box> response = await PartnersService.getBoxs(token);
+  // List<dynamic> responseList = jsonDecode(response.body);
+  // ignore: unused_local_variable
+  // print(response);
+  //     for (var responseMap in response) {
+  //       print(responseMap.description);
+  //     }
+  //   } catch (e) {
+  //     print('Error: $e');
+  //   }
+  // }
 
   Future<void> readToken() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     setState(() {
       token = prefs.getString('token') ?? "0";
     });
-    await getPartnerBoxs(); // Appeler getPartnerBoxs() comme Future
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(child: Text("Boxs")),
+    return FutureBuilder<List<Box>>(
+      future: PartnersService.getBoxs(token),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return ListView.builder(
+            itemCount: snapshot.data?.length,
+            itemBuilder: (context, index) {
+              final box = snapshot.data![index];
+              return
+                  // ListTile(
+                  //   title: Text(box.title),
+                  //   subtitle: Text(box.category),
+                  // );
+                  BoxCard(
+                image: box.image,
+                title: box.title,
+                description: box.description,
+                newPrice: box.newprice,
+              );
+            },
+          );
+        } else if (snapshot.hasError) {
+          return Text('Error: ${snapshot.error}');
+        } else {
+          return const CircularProgressIndicator();
+        }
+      },
     );
   }
 }
