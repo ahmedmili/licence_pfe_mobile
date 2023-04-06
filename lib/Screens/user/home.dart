c97eb2f25c29d97827335b3d2a23115d2288d06e
mili ahmed <ahmedmili76@gmail.com>
2023-04-06 03:42:06 +0100

    [Feat] make home page scrollable and add new boxs lists

--- lib/Screens/user/home.dart
+++ lib/Screens/user/home.dart
@@ -53,108 +53,129 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: FutureBuilder<String>(
-      future: readToken(),
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          return FutureBuilder<List<Box>>(
-            future: UserService.getAvailableBoxs(snapshot.data!),
-            builder: (context, snapshot) {
-              // print(snapshot.data);
-              if (snapshot.hasData) {
-                return Column(
-                  children: [
-                    const SizedBox(height: 40.0),
-                    SearchField(),
-                    const SizedBox(height: 30.0),
-                    Container(
-                      padding: EdgeInsets.only(left: 20.0, right: 15.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: <Widget>[
-                          const Text(
-                            "All boxes available",
-                            style: TextStyle(
-                                fontSize: 18, fontWeight: FontWeight.bold),
-                          ),
-                          Row(
+      body: ListView(
+        scrollDirection: Axis.vertical,
+        children: [
+          Column(
+            children: [
+              FutureBuilder<String>(
+                future: readToken(),
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return FutureBuilder<List<Box>>(
+                      future: UserService.getAvailableBoxs(snapshot.data!),
+                      builder: (context, snapshot) {
+                        if (snapshot.hasData) {
+                          return Column(
                             children: [
-                              Text(
-                                "View all",
-                                style: TextStyle(
-                                    fontSize: 18,
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.green[800]),
+                              const SizedBox(height: 40.0),
+                              const SearchField(),
+                              const SizedBox(height: 30.0),
+                              Container(
+                                padding: const EdgeInsets.only(
+                                    left: 20.0, right: 15.0),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: <Widget>[
+                                    const Text(
+                                      "All boxes available",
+                                      style: TextStyle(
+                                          fontSize: 18,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    Row(
+                                      children: [
+                                        Text(
+                                          "View all",
+                                          style: TextStyle(
+                                              fontSize: 18,
+                                              fontWeight: FontWeight.bold,
+                                              color: Colors.green[800]),
+                                        ),
+                                        Icon(
+                                          Icons.arrow_forward,
+                                          color: Colors.green[800],
+                                        ),
+                                      ],
+                                    )
+                                  ],
+                                ),
                               ),
-                              Icon(
-                                Icons.arrow_forward,
-                                color: Colors.green[800],
+                              const SizedBox(height: 15.0),
+                              SizedBox(
+                                height: 200, // or any other fixed height
+                                child: BoxScreen(items: snapshot.data!),
+                              ),
+                              Container(
+                                margin: const EdgeInsets.only(bottom: 10),
+                                padding: const EdgeInsets.only(
+                                    left: 20.0, right: 15.0),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: <Widget>[
+                                    const Text(
+                                      "Recommended for you",
+                                      style: TextStyle(
+                                          fontSize: 18,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    Row(
+                                      children: [
+                                        Text(
+                                          "View all",
+                                          style: TextStyle(
+                                              fontSize: 18,
+                                              fontWeight: FontWeight.bold,
+                                              color: Colors.green[800]),
+                                        ),
+                                        Icon(
+                                          Icons.arrow_forward,
+                                          color: Colors.green[800],
+                                        ),
+                                      ],
+                                    )
+                                  ],
+                                ),
+                              ),
+                              const SizedBox(height: 15.0),
+                              SizedBox(
+                                height: 200, // or any other fixed height
+                                child: BoxScreen(items: snapshot.data!),
                               ),
                             ],
-                          )
-                        ],
+                          );
+                        } else if (snapshot.hasError) {
+                          return Text('Error: ${snapshot.error}');
+                        } else {
+                          return Center(
+                            child: CircularProgressIndicator(
+                              valueColor: AlwaysStoppedAnimation<Color>(
+                                  Colors.green[800]!),
+                              strokeWidth: 5,
+                            ),
+                          );
+                        }
+                      },
+                    );
+                  } else if (snapshot.hasError) {
+                    return Text('Error: ${snapshot.error}');
+                  } else {
+                    return Center(
+                      child: CircularProgressIndicator(
+                        valueColor:
+                            AlwaysStoppedAnimation<Color>(Colors.green[800]!),
+                        strokeWidth: 5,
                       ),
-                    ),
-                    const SizedBox(height: 15.0),
-                    Expanded(
-                      child: BoxScreen(items: snapshot.data!),
-                    ),
-                    Container(
-                      margin: EdgeInsets.only(bottom: 220),
-                      padding: EdgeInsets.only(left: 20.0, right: 15.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: <Widget>[
-                          Text(
-                            "Recommended for you",
-                            style: TextStyle(
-                                fontSize: 18, fontWeight: FontWeight.bold),
-                          ),
-                          Row(
-                            children: [
-                              Text(
-                                "View all",
-                                style: TextStyle(
-                                    fontSize: 18,
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.green[800]),
-                              ),
-                              Icon(
-                                Icons.arrow_forward,
-                                color: Colors.green[800],
-                              ),
-                            ],
-                          )
-                        ],
-                      ),
-                    ),
-                  ],
-                );
-              } else if (snapshot.hasError) {
-                return Text('Error: ${snapshot.error}');
-              } else {
-                return Center(
-                  child: CircularProgressIndicator(
-                    valueColor:
-                        AlwaysStoppedAnimation<Color>(Colors.green[800]!),
-                    strokeWidth: 5,
-                  ),
-                );
-                ;
-              }
-            },
-          );
-        } else if (snapshot.hasError) {
-          return Text('Error: ${snapshot.error}');
-        } else {
-          return Center(
-            child: CircularProgressIndicator(
-              valueColor: AlwaysStoppedAnimation<Color>(Colors.green[800]!),
-              strokeWidth: 5,
-            ),
-          );
-        }
-      },
-    ));
+                    );
+                  }
+                },
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
