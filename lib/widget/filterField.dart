62f5d073cdb76343444b2916fc33d3250a5f4db4
mahakchaich <mahakchaich105@gmail.com>
2023-04-13 04:20:14 +0100

    fixed

--- lib/widget/filterField.dart
+++ lib/widget/filterField.dart
@@ -1,5 +1,8 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:saverapp/Screens/user/filter.dart';
+import 'package:saverapp/Screens/user/tour.dart';
 
 class FilterField extends StatefulWidget {
   const FilterField({super.key});
@@ -11,6 +14,43 @@ class FilterField extends StatefulWidget {
 class _FilterFieldState extends State<FilterField> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(
+                height: 30,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 150),
+                    child: const Text(
+                      "Filters",
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                    ),
+                  ),
+                  GestureDetector(
+                      onTap: () {
+                        Navigator.pop(context);
+                      },
+                      child: Icon(Icons.close)),
+                ],
+              ),
+              SizedBox(height: 40),
+              Text(
+                "Food Categories",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
