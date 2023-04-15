b2d43a4cdb675eca56497564a70aed3e9e72ab1c
mahakchaich <mahakchaich105@gmail.com>
2023-04-15 06:18:44 +0100

    fixed

--- lib/Screens/user/order.dart
+++ lib/Screens/user/order.dart
@@ -1,6 +1,10 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:saverapp/Screens/partner/home_.dart';
+
+import '../../widget/partnerDetails.dart';
 
 class OrderScreen extends StatefulWidget {
   const OrderScreen({super.key});
@@ -40,9 +44,9 @@ class _OrderScreenState extends State<OrderScreen> {
       body: Stack(
         children: <Widget>[
           Container(
-            height: 70,
+            height: 450,
             width: 450,
-            padding: const EdgeInsets.only(left: 10.0, right: 10.0),
+            padding: const EdgeInsets.only(left: 15.0, right: 15.0, top: 30),
             child: Material(
               elevation: 5.0,
               borderRadius: BorderRadius.circular(20.0),
@@ -57,33 +61,123 @@ class _OrderScreenState extends State<OrderScreen> {
                     Row(
                       children: [
                         Icon(
-                          Icons.location_on,
+                          CupertinoIcons.cube_box,
                           color: Colors.green[800],
                         ),
                         const SizedBox(
                           width: 5,
                         ),
-                        Text(
-                          "Address",
+                        const Text(
+                          "Title Box",
                           style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              color: Colors.green[800],
-                              fontSize: 16),
-                        ),
-                        const SizedBox(
-                          width: 10,
+                              fontWeight: FontWeight.bold, fontSize: 18),
                         ),
                       ],
                     ),
+                    const SizedBox(height: 10),
                     Padding(
-                      padding: const EdgeInsets.only(left: 30),
-                      child: Text(
-                        "Less than 6 Km",
-                        style: TextStyle(
-                            //fontWeight: FontWeight.bold,
-                            color: Colors.green[800],
-                            fontSize: 14),
+                      padding: const EdgeInsets.all(20.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Padding(
+                            padding: EdgeInsets.only(left: 10),
+                            child: Text(
+                              "Total",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 14),
+                            ),
+                          ),
+                          const Text(
+                            "price",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 14),
+                          ),
+                        ],
                       ),
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.only(right: 15.0),
+                      child: Divider(
+                        color: Colors.grey,
+                        thickness: 1,
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (BuildContext context) =>
+                                  const PartnerDetails(),
+                            ));
+                      },
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Row(
+                                children: [
+                                  const SizedBox(height: 20),
+                                  Icon(
+                                    Icons.location_on_outlined,
+                                    color: Colors.green[800],
+                                  ),
+                                  const SizedBox(width: 5),
+                                  const Text(
+                                    "adresse commerce",
+                                    style: TextStyle(
+                                        fontSize: 14,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                ],
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(left: 30),
+                                child: Text(
+                                  "More information about ",
+                                  style: TextStyle(
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.green[800]),
+                                ),
+                              ),
+                            ],
+                          ),
+                          const Padding(
+                            padding: EdgeInsets.all(20.0),
+                            child: Icon(Icons.chevron_right),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    const Padding(
+                      padding: EdgeInsets.only(right: 15.0),
+                      child: Divider(
+                        color: Colors.grey,
+                        thickness: 1,
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: [
+                            const Icon(Icons.access_alarm),
+                            const SizedBox(width: 5),
+                            Text(
+                              'collect : Date',
+                              style: const TextStyle(
+                                  fontSize: 14, fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        ),
+                      ],
                     ),
                   ],
                 ),
@@ -91,6 +185,31 @@ class _OrderScreenState extends State<OrderScreen> {
             ),
           ),
         ],
+      ),
+      bottomNavigationBar: Container(
+        height: 100,
+        padding:
+            const EdgeInsets.only(top: 30, bottom: 30, left: 20, right: 20),
+        decoration: BoxDecoration(
+          color: Colors.grey[200],
+        ),
+        child: MaterialButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const HomeScreen()),
+            );
+          },
+          color: Colors.green[800],
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: const Text(
+            "Click To Collect Your Box",
+            style: TextStyle(
+                color: Colors.white, fontWeight: FontWeight.bold, fontSize: 18),
+          ),
+        ),
       ),
     );
   }
