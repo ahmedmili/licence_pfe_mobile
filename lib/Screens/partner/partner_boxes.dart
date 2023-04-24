e4b97d82e523b3ead8ae709f38475829eee04747
mahakchaich <mahakchaich105@gmail.com>
2023-04-24 23:01:33 +0100

    fixed

--- lib/Screens/partner/partner_boxes.dart
+++ lib/Screens/partner/partner_boxes.dart
@@ -35,7 +35,6 @@ class _PartnerBoxesState extends State<PartnerBoxes> {
                 future: PartnersService.getPartnerBoxesAccepted(),
                 builder: (context, snapshot) {
                   if (snapshot.hasData) {
-                    // print(snapshot.data![0].id);
                     return Column(
                       children: [
                         const SizedBox(
@@ -77,41 +76,6 @@ class _PartnerBoxesState extends State<PartnerBoxes> {
                           height: 200, // or any other fixed height
                           child: BoxScreenPartner(items: snapshot.data!),
                         ),
-                        // Container(
-                        //   margin: const EdgeInsets.only(bottom: 10),
-                        //   padding:
-                        //       const EdgeInsets.only(left: 20.0, right: 15.0),
-                        //   child: Row(
-                        //     mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        //     children: <Widget>[
-                        //       const Text(
-                        //         "Pending boxes",
-                        //         style: TextStyle(
-                        //             fontSize: 18, fontWeight: FontWeight.bold),
-                        //       ),
-                        //       Row(
-                        //         children: [
-                        //           Text(
-                        //             "View all",
-                        //             style: TextStyle(
-                        //                 fontSize: 18,
-                        //                 fontWeight: FontWeight.bold,
-                        //                 color: Colors.green[800]),
-                        //           ),
-                        //           Icon(
-                        //             Icons.arrow_forward,
-                        //             color: Colors.green[800],
-                        //           ),
-                        //         ],
-                        //       )
-                        //     ],
-                        //   ),
-                        // ),
-                        // const SizedBox(height: 15.0),
-                        // SizedBox(
-                        //   height: 200, // or any other fixed height
-                        //   child: BoxScreenPartner(items: snapshot.data!),
-                        // ),
                       ],
                     );
                   } else if (snapshot.hasError) {
@@ -131,7 +95,6 @@ class _PartnerBoxesState extends State<PartnerBoxes> {
                 future: PartnersService.getPartnerBoxesPending(),
                 builder: (context, snapshot) {
                   if (snapshot.hasData) {
-                    // print(snapshot.data![0].id);
                     return Column(
                       children: [
                         const SizedBox(height: 15),
@@ -169,41 +132,174 @@ class _PartnerBoxesState extends State<PartnerBoxes> {
                           height: 200, // or any other fixed height
                           child: BoxScreenPartner(items: snapshot.data!),
                         ),
-                        // Container(
-                        //   margin: const EdgeInsets.only(bottom: 10),
-                        //   padding:
-                        //       const EdgeInsets.only(left: 20.0, right: 15.0),
-                        //   child: Row(
-                        //     mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        //     children: <Widget>[
-                        //       const Text(
-                        //         "Pending boxes",
-                        //         style: TextStyle(
-                        //             fontSize: 18, fontWeight: FontWeight.bold),
-                        //       ),
-                        //       Row(
-                        //         children: [
-                        //           Text(
-                        //             "View all",
-                        //             style: TextStyle(
-                        //                 fontSize: 18,
-                        //                 fontWeight: FontWeight.bold,
-                        //                 color: Colors.green[800]),
-                        //           ),
-                        //           Icon(
-                        //             Icons.arrow_forward,
-                        //             color: Colors.green[800],
-                        //           ),
-                        //         ],
-                        //       )
-                        //     ],
-                        //   ),
-                        // ),
-                        // const SizedBox(height: 15.0),
-                        // SizedBox(
-                        //   height: 200, // or any other fixed height
-                        //   child: BoxScreenPartner(items: snapshot.data!),
-                        // ),
+                      ],
+                    );
+                  } else if (snapshot.hasError) {
+                    return Text('Error: ${snapshot.error}');
+                  } else {
+                    return Center(
+                      child: CircularProgressIndicator(
+                        valueColor:
+                            AlwaysStoppedAnimation<Color>(Colors.green[800]!),
+                        strokeWidth: 5,
+                      ),
+                    );
+                  }
+                },
+              ),
+              FutureBuilder<List<Box>>(
+                future: PartnersService.getPartnerBoxsFinished(),
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return Column(
+                      children: [
+                        const SizedBox(height: 15),
+                        Container(
+                          padding:
+                              const EdgeInsets.only(left: 20.0, right: 15.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                              const Text(
+                                "Finished boxes",
+                                style: TextStyle(
+                                    fontSize: 18, fontWeight: FontWeight.bold),
+                              ),
+                              Row(
+                                children: [
+                                  Text(
+                                    "View all",
+                                    style: TextStyle(
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.green[800]),
+                                  ),
+                                  Icon(
+                                    Icons.arrow_forward,
+                                    color: Colors.green[800],
+                                  ),
+                                ],
+                              )
+                            ],
+                          ),
+                        ),
+                        const SizedBox(height: 15.0),
+                        SizedBox(
+                          height: 200, // or any other fixed height
+                          child: BoxScreenPartner(items: snapshot.data!),
+                        ),
+                      ],
+                    );
+                  } else if (snapshot.hasError) {
+                    return Text('Error: ${snapshot.error}');
+                  } else {
+                    return Center(
+                      child: CircularProgressIndicator(
+                        valueColor:
+                            AlwaysStoppedAnimation<Color>(Colors.green[800]!),
+                        strokeWidth: 5,
+                      ),
+                    );
+                  }
+                },
+              ),
+              FutureBuilder<List<Box>>(
+                future: PartnersService.getPartnerBoxsExpired(),
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return Column(
+                      children: [
+                        const SizedBox(height: 15),
+                        Container(
+                          padding:
+                              const EdgeInsets.only(left: 20.0, right: 15.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                              const Text(
+                                "Expired boxes",
+                                style: TextStyle(
+                                    fontSize: 18, fontWeight: FontWeight.bold),
+                              ),
+                              Row(
+                                children: [
+                                  Text(
+                                    "View all",
+                                    style: TextStyle(
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.green[800]),
+                                  ),
+                                  Icon(
+                                    Icons.arrow_forward,
+                                    color: Colors.green[800],
+                                  ),
+                                ],
+                              )
+                            ],
+                          ),
+                        ),
+                        const SizedBox(height: 15.0),
+                        SizedBox(
+                          height: 200, // or any other fixed height
+                          child: BoxScreenPartner(items: snapshot.data!),
+                        ),
+                      ],
+                    );
+                  } else if (snapshot.hasError) {
+                    return Text('Error: ${snapshot.error}');
+                  } else {
+                    return Center(
+                      child: CircularProgressIndicator(
+                        valueColor:
+                            AlwaysStoppedAnimation<Color>(Colors.green[800]!),
+                        strokeWidth: 5,
+                      ),
+                    );
+                  }
+                },
+              ),
+              FutureBuilder<List<Box>>(
+                future: PartnersService.getPartnerBoxsRejected(),
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return Column(
+                      children: [
+                        const SizedBox(height: 15),
+                        Container(
+                          padding:
+                              const EdgeInsets.only(left: 20.0, right: 15.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                              const Text(
+                                "Rejected boxes",
+                                style: TextStyle(
+                                    fontSize: 18, fontWeight: FontWeight.bold),
+                              ),
+                              Row(
+                                children: [
+                                  Text(
+                                    "View all",
+                                    style: TextStyle(
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.green[800]),
+                                  ),
+                                  Icon(
+                                    Icons.arrow_forward,
+                                    color: Colors.green[800],
+                                  ),
+                                ],
+                              )
+                            ],
+                          ),
+                        ),
+                        const SizedBox(height: 15.0),
+                        SizedBox(
+                          height: 200, // or any other fixed height
+                          child: BoxScreenPartner(items: snapshot.data!),
+                        ),
                       ],
                     );
                   } else if (snapshot.hasError) {
--- lib/Services/partners.dart
+++ lib/Services/partners.dart
@@ -87,4 +87,122 @@ class PartnersService {
       throw Exception('Failed to fetch partner boxes');
     }
   }
+
+  static Future<List<Box>> getPartnerBoxsExpired() async {
+    final token = controller.token;
+    print(token);
+    final url = Uri.parse('${baseURL}partner/getPartnerBoxsExpired');
+    final response = await http.get(
+      url,
+      headers: {
+        "Content-Type": "application/json",
+        'Authorization': 'Bearer $token'
+      },
+    );
+    print(response.body);
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body)["Boxs"];
+      final List<Box> box = [];
+      for (int i = 0; i < data.length; i++) {
+        final newBox = Box(
+          // Add fields here to create the new Box object
+          id: data[i]['id'],
+          description: data[i]['description'],
+          category: data[i]['category'],
+          newprice: data[i]['newprice'],
+          startdate: data[i]['startdate'],
+          enddate: data[i]['enddate'],
+          quantity: data[i]['quantity'],
+          remaining_quantity: data[i]['remaining_quantity'],
+          image: data[i]['image'],
+          partnerId: data[i]['partner_id'],
+          title: data[i]['title'],
+          oldprice: data[i]['oldprice'],
+        );
+
+        box.add(newBox);
+      }
+      return box;
+    } else {
+      throw Exception('Failed to fetch partner boxes');
+    }
+  }
+
+  static Future<List<Box>> getPartnerBoxsFinished() async {
+    final token = controller.token;
+    final url = Uri.parse('${baseURL}partner/getPartnerBoxsFinished');
+    final response = await http.get(
+      url,
+      headers: {
+        "Content-Type": "application/json",
+        'Authorization': 'Bearer $token'
+      },
+    );
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body)["Boxs"];
+      final List<Box> box = [];
+      for (int i = 0; i < data.length; i++) {
+        final newBox = Box(
+          // Add fields here to create the new Box object
+          id: data[i]['id'],
+          description: data[i]['description'],
+          category: data[i]['category'],
+          newprice: data[i]['newprice'],
+          startdate: data[i]['startdate'],
+          enddate: data[i]['enddate'],
+          quantity: data[i]['quantity'],
+          remaining_quantity: data[i]['remaining_quantity'],
+          image: data[i]['image'],
+          partnerId: data[i]['partner_id'],
+          title: data[i]['title'],
+          oldprice: data[i]['oldprice'],
+        );
+
+        box.add(newBox);
+      }
+      return box;
+    } else {
+      throw Exception('Failed to fetch partner boxes');
+    }
+  }
+
+  static Future<List<Box>> getPartnerBoxsRejected() async {
+    final token = controller.token;
+    print(token);
+    final url = Uri.parse('${baseURL}partner/getPartnerBoxsRejected');
+    final response = await http.get(
+      url,
+      headers: {
+        "Content-Type": "application/json",
+        'Authorization': 'Bearer $token'
+      },
+    );
+    print(response.body);
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body)["Boxs"];
+      final List<Box> box = [];
+      for (int i = 0; i < data.length; i++) {
+        final newBox = Box(
+          // Add fields here to create the new Box object
+          id: data[i]['id'],
+          description: data[i]['description'],
+          category: data[i]['category'],
+          newprice: data[i]['newprice'],
+          startdate: data[i]['startdate'],
+          enddate: data[i]['enddate'],
+          quantity: data[i]['quantity'],
+          remaining_quantity: data[i]['remaining_quantity'],
+          image: data[i]['image'],
+          partnerId: data[i]['partner_id'],
+          title: data[i]['title'],
+          oldprice: data[i]['oldprice'],
+        );
+
+        box.add(newBox);
+      }
+      return box;
+    } else {
+      throw Exception('Failed to fetch partner boxes');
+    }
+  }
 }
