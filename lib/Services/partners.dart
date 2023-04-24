1778c2a20e3cc0444b5d7fa88d73a297f6cc04e1
mahakchaich <mahakchaich105@gmail.com>
2023-04-24 13:34:49 +0100

    fixed

--- lib/Services/partners.dart
+++ lib/Services/partners.dart
@@ -10,24 +10,30 @@ final GlobalController controller = Get.find<GlobalController>();
 class PartnersService {
   static Future<List<Box>> getPartnerBoxes() async {
     final token = controller.token;
+    if (token == null) {
+      throw Exception('Token is null');
+    }
     final url = Uri.parse('${baseURL}partner/partnerboxes');
-    print(token);
     final response = await http.get(
       url,
       headers: {
         "Content-Type": "application/json",
-        'Authorization': 'Bearer $token'
+        if (token != null) 'Authorization': 'Bearer $token'
       },
     );
-    print(response.body);
     if (response.statusCode == 200) {
-      List<dynamic> data = jsonDecode(response.body);
-
-      final box = List<Box>.from(
-        data.map((boxJson) => Box.fromJson(boxJson)),
-      );
-
-      return box;
+      final data = jsonDecode(response.body);
+      if (data is List<dynamic>) {
+        final box = List<Box>.from(
+          data.map((boxJson) => Box.fromJson(boxJson)),
+        );
+        return box;
+      } else if (data is Map<String, dynamic>) {
+        final box = Box.fromJson(data);
+        return [box];
+      } else {
+        throw Exception('Failed to fetch partner boxes');
+      }
     } else {
       throw Exception('Failed to fetch partner boxes');
     }
