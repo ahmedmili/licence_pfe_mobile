421751652c32f835ddb3c08d91489d6b217c341e
mili ahmed <ahmedmili76@gmail.com>
2023-05-01 12:29:05 +0100

    [Fix] logout delete data

--- lib/Screens/login.dart
+++ lib/Screens/login.dart
@@ -101,6 +101,7 @@ class _LoginScreenState extends State<LoginScreen> {
 
   @override
   Widget build(BuildContext context) {
+    print("login page token = ${controller.token}");
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.transparent,
