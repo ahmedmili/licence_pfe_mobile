eec1981501fedcd9e2948dd13d9ec97a8d1e9eba
mahakchaich <mahakchaich105@gmail.com>
2023-05-02 12:42:11 +0100

    fixed

--- lib/Screens/welcome.dart
+++ lib/Screens/welcome.dart
@@ -15,93 +15,95 @@ class _WelcomeState extends State<Welcome> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          const SizedBox(
-            height: 60,
-          ),
-          Center(
-            child: Image.asset(
-              "assets/images/food.png",
-              width: 200,
-              height: 200,
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            const SizedBox(
+              height: 60,
             ),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          Text("Welcome to SaverFood",
-              style: GoogleFonts.bebasNeue(fontSize: 40)),
-          const SizedBox(
-            height: 10,
-          ),
-          const Text(
-            "Let's take you with us !",
-            style: TextStyle(
-              fontSize: 20,
-            ),
-          ),
-          const SizedBox(
-            height: 50,
-          ),
-          RoundedButton(
-            btnText: 'LOGIN',
-            onBtnPressed: () => Get.toNamed("/login"),
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          Container(
-            height: 60,
-            width: 320,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              border: Border.all(
-                color: Colors.green.shade800,
-                width: 2.0,
-                style: BorderStyle.solid,
+            Center(
+              child: Image.asset(
+                "assets/images/food.png",
+                width: 200,
+                height: 200,
               ),
             ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                GestureDetector(
-                  onTap: () => Get.toNamed("/registerPartner"),
-                  child: Padding(
-                    padding: const EdgeInsets.only(right: 5),
-                    child: Text(
-                      "Register As Partner",
-                      style: TextStyle(
-                          fontSize: 14,
-                          color: Colors.green.shade800,
-                          fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                ),
-                VerticalDivider(
-                  color: Colors.green[800],
-                  width: 20.0,
-                ),
-                GestureDetector(
-                  onTap: () {
-                    Get.toNamed("/registerUser");
-                  },
-                  child: Padding(
-                    padding: const EdgeInsets.only(right: 10.0),
-                    child: Text(
-                      "Register As User",
-                      style: TextStyle(
-                          fontSize: 14,
-                          color: Colors.green.shade800,
-                          fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                ),
-              ],
+            const SizedBox(
+              height: 20,
             ),
-          ),
-        ],
+            Text("Welcome to SaverFood",
+                style: GoogleFonts.bebasNeue(fontSize: 40)),
+            const SizedBox(
+              height: 10,
+            ),
+            const Text(
+              "Let's take you with us !",
+              style: TextStyle(
+                fontSize: 20,
+              ),
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            RoundedButton(
+              btnText: 'LOGIN',
+              onBtnPressed: () => Get.toNamed("/login"),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Container(
+              height: 60,
+              width: 320,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                border: Border.all(
+                  color: Colors.green.shade800,
+                  width: 2.0,
+                  style: BorderStyle.solid,
+                ),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  GestureDetector(
+                    onTap: () => Get.toNamed("/registerPartner"),
+                    child: Padding(
+                      padding: const EdgeInsets.only(right: 5),
+                      child: Text(
+                        "Register As Partner",
+                        style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.green.shade800,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                  VerticalDivider(
+                    color: Colors.green[800],
+                    width: 20.0,
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      Get.toNamed("/registerUser");
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.only(right: 10.0),
+                      child: Text(
+                        "Register As User",
+                        style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.green.shade800,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
