20e45d8de204e76e11bd151641cbd48d593d106b
mahakchaich <mahakchaich105@gmail.com>
2023-05-03 11:34:13 +0100

    fixed

--- lib/widget/partnerDetails.dart
+++ lib/widget/partnerDetails.dart
@@ -24,193 +24,102 @@ class _PartnerDetailsState extends State<PartnerDetails> {
       body: Stack(
         children: <Widget>[
           Container(
-            height: 450,
-            width: 450,
-            padding: const EdgeInsets.only(left: 15.0, right: 15.0, top: 0),
+            height: 500,
+            width: 400,
+            padding: const EdgeInsets.only(left: 35.0, right: 25.0, top: 0),
             child: Material(
               elevation: 5.0,
               borderRadius: BorderRadius.circular(20.0),
-              child: Padding(
-                padding: const EdgeInsets.only(left: 10),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    const SizedBox(
-                      height: 15,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Container(
-                          height: 60,
-                          width: 150,
-                          decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            border: Border.all(
-                              color: Colors.black,
-                              width: 2.0,
-                            ),
-                          ),
-                          child: CircleAvatar(
-                            radius: 20,
-                            backgroundImage: NetworkImage(
-                              "http://10.0.2.2:8000/storage/partner_imgs/",
-                              headers: {
-                                "Keep-Alive": "timeout=20",
-                              },
-                            ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  const SizedBox(height: 20),
+                  Row(
+                    children: [
+                      Container(
+                        height: 60,
+                        width: 100,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          border: Border.all(
+                            color: Colors.black,
+                            width: 2.0,
                           ),
                         ),
-                        Text("name"),
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        Icon(
-                          CupertinoIcons.cube_box,
-                          color: Colors.green[800],
-                        ),
-                        const SizedBox(
-                          width: 5,
-                        ),
-                        const Text(
-                          "Title Box",
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold, fontSize: 18),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(height: 10),
-                    Padding(
-                      padding: const EdgeInsets.all(20.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: const [
-                          Padding(
-                            padding: EdgeInsets.only(left: 10),
-                            child: Text(
-                              "Total",
-                              style: TextStyle(
-                                  fontWeight: FontWeight.bold, fontSize: 14),
-                            ),
+                        child: CircleAvatar(
+                          radius: 20,
+                          backgroundImage: NetworkImage(
+                            "http://10.0.2.2:8000/storage/partner_imgs/",
+                            headers: {
+                              "Keep-Alive": "timeout=20",
+                            },
                           ),
-                          Text(
-                            "price",
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 14),
-                          ),
-                        ],
+                        ),
                       ),
-                    ),
-                    const Padding(
-                      padding: EdgeInsets.only(right: 15.0),
-                      child: Divider(
-                        color: Colors.grey,
-                        thickness: 1,
+                      Text(
+                        "name",
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
                       ),
-                    ),
-                    const SizedBox(height: 10),
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (BuildContext context) =>
-                                  const PartnerDetails(),
-                            ));
-                      },
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Row(
-                                children: [
-                                  const SizedBox(height: 20),
-                                  Icon(
-                                    Icons.location_on_outlined,
-                                    color: Colors.green[800],
-                                  ),
-                                  const SizedBox(width: 5),
-                                  const Text(
-                                    "adresse commerce",
-                                    style: TextStyle(
-                                        fontSize: 14,
-                                        fontWeight: FontWeight.bold),
-                                  ),
-                                ],
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.only(left: 30),
-                                child: Text(
-                                  "More information about ",
-                                  style: TextStyle(
-                                      fontSize: 14,
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.green[800]),
-                                ),
-                              ),
-                            ],
-                          ),
-                          const Padding(
-                            padding: EdgeInsets.all(20.0),
-                            child: Icon(Icons.chevron_right),
-                          ),
-                        ],
+                    ],
+                  ),
+                  const SizedBox(height: 20),
+                  Row(
+                    children: [
+                      const SizedBox(
+                        width: 20,
                       ),
-                    ),
-                    const SizedBox(height: 10),
-                    const Padding(
-                      padding: EdgeInsets.only(right: 15.0),
-                      child: Divider(
-                        color: Colors.grey,
-                        thickness: 1,
+                      const Text(
+                        "Category",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 18),
                       ),
-                    ),
-                    const SizedBox(height: 10),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Row(
-                          children: const [
-                            Icon(Icons.access_alarm),
-                            SizedBox(width: 5),
-                            Text(
-                              'collect : Date',
-                              style: TextStyle(
-                                  fontSize: 14, fontWeight: FontWeight.bold),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
+                    ],
+                  ),
+                  const SizedBox(height: 20),
+                  Row(
+                    children: [
+                      const SizedBox(
+                        width: 20,
+                      ),
+                      const Text(
+                        "Description",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 18),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 20),
+                  Row(
+                    children: [
+                      const SizedBox(
+                        width: 20,
+                      ),
+                      const Text(
+                        "Phone",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 18),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 20),
+                  Row(
+                    children: [
+                      const SizedBox(
+                        width: 20,
+                      ),
+                      const Text(
+                        "Email",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 18),
+                      ),
+                    ],
+                  ),
+                ],
               ),
             ),
           ),
         ],
-      ),
-      bottomNavigationBar: Container(
-        height: 100,
-        padding:
-            const EdgeInsets.only(top: 30, bottom: 30, left: 20, right: 20),
-        decoration: BoxDecoration(
-          color: Colors.grey[200],
-        ),
-        child: MaterialButton(
-          onPressed: () {},
-          color: Colors.green[800],
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(20),
-          ),
-          child: const Text(
-            "Click To Collect Your Box",
-            style: TextStyle(
-                color: Colors.white, fontWeight: FontWeight.bold, fontSize: 18),
-          ),
-        ),
       ),
     );
   }
