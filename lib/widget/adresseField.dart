eaf1147e8afe18699a6d29fe5b22aa2211f5b104
mahakchaich <mahakchaich105@gmail.com>
2023-05-03 16:56:23 +0100

    fixed

--- lib/widget/adresseField.dart
+++ lib/widget/adresseField.dart
@@ -15,56 +15,58 @@ class _AdresseFieldState extends State<AdresseField> {
   Widget build(BuildContext context) {
     return Stack(
       children: <Widget>[
-        Expanded(
-          child: Container(
-            height: 70,
-            width: 450,
-            padding: const EdgeInsets.only(left: 10.0, right: 10.0),
-            child: Material(
-              elevation: 5.0,
-              borderRadius: BorderRadius.circular(20.0),
-              child: Padding(
-                padding: const EdgeInsets.only(left: 17),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    const SizedBox(
-                      height: 15,
-                    ),
-                    Row(
-                      children: [
-                        Icon(
-                          Icons.location_on,
-                          color: Colors.green[800],
-                        ),
-                        const SizedBox(
-                          width: 5,
-                        ),
-                        Text(
-                          "Address",
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              color: Colors.green[800],
-                              fontSize: 16),
-                        ),
-                        const SizedBox(
-                          width: 10,
-                        ),
-                      ],
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 30),
-                      child: Text(
-                        "Less than 6 Km",
-                        style: TextStyle(
-                            //fontWeight: FontWeight.bold,
-                            color: Colors.green[800],
-                            fontSize: 14),
+        Container(
+          height: 70,
+          width: 450,
+          padding: const EdgeInsets.only(left: 10.0, right: 10.0),
+          child: Material(
+            elevation: 5.0,
+            borderRadius: BorderRadius.circular(20.0),
+            child: Padding(
+              padding: const EdgeInsets.only(left: 17),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.location_on,
+                        color: Colors.green[800],
                       ),
+                      const SizedBox(
+                        width: 5,
+                      ),
+                      Expanded(
+                        child: SingleChildScrollView(
+                          child: Text(
+                            widget.adress,
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.green[800],
+                                fontSize: 16),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 15,
+                      ),
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 30),
+                    child: Text(
+                      "Less than 6 Km",
+                      style: TextStyle(
+                          //fontWeight: FontWeight.bold,
+                          color: Colors.green[800],
+                          fontSize: 14),
                     ),
-                    Text(widget.adress),
-                  ],
-                ),
+                  ),
+                  //Text(widget.adress),
+                ],
               ),
             ),
           ),
