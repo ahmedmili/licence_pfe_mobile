5c5e5a7298e6935fc476cdc30699f896a4103792
mahakchaich <mahakchaich105@gmail.com>
2023-05-05 16:14:45 +0100

    fixed

--- lib/Screens/user/favorite_partner.dart
+++ lib/Screens/user/favorite_partner.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:saverapp/widget/partnerCard.dart';
 
 class FavoritePartner extends StatefulWidget {
   const FavoritePartner({super.key});
@@ -12,6 +13,13 @@ class FavoritePartner extends StatefulWidget {
 class _FavoritePartnerState extends State<FavoritePartner> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Column(
+        children: [
+          SizedBox(height: 20),
+          PartnerCard(),
+        ],
+      ),
+    );
   }
 }
