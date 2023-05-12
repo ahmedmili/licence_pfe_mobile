31fbabec2b4cabe9ac1427dd9cc6636691db8862
mili ahmed <ahmedmili76@gmail.com>
2023-05-12 14:42:21 +0100

    merge

--- lib/Services/globals.dart
+++ lib/Services/globals.dart
@@ -6,7 +6,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 import '../Models/partner.dart';
 
 const String baseURL = "http://$localhost:8000/api/";
-const String localhost = "192.168.100.34";
+const String localhost = "192.168.100.33";
 // const String baseURL = "http://$localhost:8000/api/";
 // const String localhost = "10.0.2.2";
 //const String localhost = "192.168.100.33";
