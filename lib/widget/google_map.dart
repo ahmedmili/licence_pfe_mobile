cad8477251119542ced73823dc64ca89363e6efa
mili ahmed <ahmedmili76@gmail.com>
2023-05-12 14:41:05 +0100

    [feat] map markers added

--- lib/widget/google_map.dart
+++ lib/widget/google_map.dart
@@ -1,4 +1,6 @@
 import 'dart:async';
+import 'dart:typed_data';
+import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -17,18 +19,18 @@ class MapSampleState extends State<MapSample> {
 
   final Completer<GoogleMapController> _controller =
       Completer<GoogleMapController>();
+  static LatLng _center = const LatLng(0, 0);
 
   CameraPosition _initialPosition = const CameraPosition(
     target: LatLng(0, 0),
     zoom: 14.4746,
   );
 
-  // final CameraPosition _lakePosition = const CameraPosition(
-  //     bearing: 192.8334901395799,
-  //     // target: LatLng(37.43296265331129, -122.08832357078792),
-  //     target: LatLng(37.43296265331129, -122.08832357078792),
-  //     tilt: 59.440717697143555,
-  //     zoom: 19.151926040649414);
+  final CameraPosition _lakePosition = const CameraPosition(
+      bearing: 192.8334901395799,
+      target: LatLng(37.43296265331129, -122.08832357078792),
+      tilt: 59.440717697143555,
+      zoom: 19.151926040649414);
 
   @override
   void initState() {
@@ -40,6 +42,8 @@ class MapSampleState extends State<MapSample> {
     LatLng currentLocation = LatLng(double.parse(geoController.lat.value),
         double.parse(geoController.long.value));
     setState(() {
+      _center = LatLng(double.parse(geoController.lat.value),
+          double.parse(geoController.long.value)); // current position
       _initialPosition = CameraPosition(
         target: currentLocation,
         zoom: 14.4746,
@@ -47,26 +51,60 @@ class MapSampleState extends State<MapSample> {
     });
   }
 
+  final Set<Marker> _markers = {
+    const Marker(
+      markerId: MarkerId('great_mosque'),
+      position: LatLng(35.8288, 10.6396),
+      infoWindow: InfoWindow(title: 'Great Mosque of Sousse'),
+    ),
+    Marker(
+      icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueCyan),
+      markerId: const MarkerId('user_position'),
+      position: _center,
+      infoWindow: const InfoWindow(title: 'your poseition'),
+    ),
+  };
+
+  final Set<Circle> _circles = {
+    Circle(
+      circleId: const CircleId('center'),
+      center: _center,
+      radius: 2000, // In meters
+      fillColor: Colors.blue.withOpacity(0.1),
+      strokeColor: Colors.blue.withOpacity(0.5),
+      strokeWidth: 2,
+    ),
+  };
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: GoogleMap(
         mapType: MapType.normal,
         initialCameraPosition: _initialPosition,
+        markers: _markers,
         onMapCreated: (GoogleMapController controller) {
           _controller.complete(controller);
         },
+        cameraTargetBounds: CameraTargetBounds(
+          LatLngBounds(
+            southwest:
+                const LatLng(35.8011, 10.6092), // Southwest corner of Sousse
+            northeast:
+                const LatLng(35.8881, 10.6924), // Northeast corner of Sousse
+          ),
+        ),
+        circles: _circles,
       ),
-      // floatingActionButton: FloatingActionButton.extended(
-      //   onPressed: _goToTheLake,
-      //   label: const Text('To the lake!'),
-      //   icon: const Icon(Icons.directions_boat),
-      // ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: _goToTheLake,
+        label: const Text('To the lake!'),
+        icon: const Icon(Icons.directions_boat),
+      ),
     );
   }
 
-  // Future<void> _goToTheLake() async {
-  //   final GoogleMapController controller = await _controller.future;
-  //   controller.animateCamera(CameraUpdate.newCameraPosition(_lakePosition));
-  // }
+  Future<void> _goToTheLake() async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(_lakePosition));
+  }
 }
