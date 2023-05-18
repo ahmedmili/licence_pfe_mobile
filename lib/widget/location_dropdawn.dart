58a8127a835dbb07cffb8c6a82d5a2c673f3e9be
mahakchaich <mahakchaich105@gmail.com>
2023-05-18 14:03:45 +0100

    fix

--- lib/widget/location_dropdawn.dart
+++ lib/widget/location_dropdawn.dart
@@ -81,35 +81,42 @@ class _PopupLocationMenu extends State<PopupLocationMenu> {
             showDialog(
               context: context,
               builder: (BuildContext context) {
-                return AlertDialog(
-                  title: const Text('Select Position'),
-                  content: GoogleMap(
-                    initialCameraPosition: const CameraPosition(
-                      target: LatLng(37.42796133580664, -122.085749655962),
-                      zoom: 14,
+                return FractionallySizedBox(
+                  widthFactor: 1.5, // Adjust the width as needed
+                  heightFactor: 1, // Adjust the height as needed
+                  child: AlertDialog(
+                    title: Text(
+                      'Select Position',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.green[800],
+                      ),
                     ),
-                    onTap: (LatLng latLng) async {
-                      List<Placemark> placemarks =
-                          await placemarkFromCoordinates(
-                        latLng.latitude,
-                        latLng.longitude,
-                      );
-
-                      String address = placemarks[0].name.toString();
-                      geoController.adress.value = address;
-                      geoController.long.value = latLng.longitude.toString();
-                      geoController.lat.value = latLng.latitude.toString();
-                      Get.back();
-                    },
-                  ),
-                  actions: <Widget>[
-                    TextButton(
-                      child: const Text('Close'),
-                      onPressed: () {
-                        Get.back();
+                    content: GoogleMap(
+                      initialCameraPosition: const CameraPosition(
+                        target: LatLng(37.42796133580664, -122.085749655962),
+                        zoom: 14,
+                      ),
+                      onTap: (LatLng latLng) async {
+                        // Rest of your code
                       },
                     ),
-                  ],
+                    actions: <Widget>[
+                      TextButton(
+                        child: Text(
+                          'SAVE',
+                          style: TextStyle(
+                              fontSize: 20,
+                              color: Colors.green.shade800,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        onPressed: () {
+                          Get.back();
+                        },
+                      ),
+                    ],
+                  ),
                 );
               },
             );
