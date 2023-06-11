ecc92c0d3ab078940f4002b19cca1e27e2b06aa5
mahakchaich <mahakchaich105@gmail.com>
2023-06-11 13:31:32 +0100

    test

--- lib/widget/DoughnutChart.dart
+++ lib/widget/DoughnutChart.dart
@@ -73,50 +73,60 @@ class _DoughnutChartState extends State<DoughnutChart> {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Row(
-        children: [
-          Expanded(
-            child: SfCircularChart(
-              series: <CircularSeries>[
-                DoughnutSeries<ChartData, String>(
-                  dataSource: chartData,
-                  xValueMapper: (ChartData data, _) => data.category,
-                  yValueMapper: (ChartData data, _) => data.value,
-                  dataLabelSettings: DataLabelSettings(
-                    isVisible: true,
-                  ),
-                  pointColorMapper: (ChartData data, _) {
-                    int index = chartData.indexOf(data) % categoryColors.length;
-                    return categoryColors[index];
-                  },
-                ),
-              ],
-            ),
-          ),
-          Container(
-            width: 100,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: chartData.map((data) {
-                int index = chartData.indexOf(data) % categoryColors.length;
-                return Row(
-                  children: [
-                    Container(
-                      width: 10,
-                      height: 10,
-                      margin: EdgeInsets.only(right: 5),
-                      color: categoryColors[index],
+    if (chartData.isEmpty) {
+      return Center(
+        child: CircularProgressIndicator(
+          valueColor: AlwaysStoppedAnimation<Color>(Color(0xFFB2CCFF)),
+          strokeWidth: 5,
+        ),
+      );
+    } else {
+      return Center(
+        child: Row(
+          children: [
+            Expanded(
+              child: SfCircularChart(
+                series: <CircularSeries>[
+                  DoughnutSeries<ChartData, String>(
+                    dataSource: chartData,
+                    xValueMapper: (ChartData data, _) => data.category,
+                    yValueMapper: (ChartData data, _) => data.value,
+                    dataLabelSettings: DataLabelSettings(
+                      isVisible: true,
                     ),
-                    Text(data.category),
-                  ],
-                );
-              }).toList(),
+                    pointColorMapper: (ChartData data, _) {
+                      int index =
+                          chartData.indexOf(data) % categoryColors.length;
+                      return categoryColors[index];
+                    },
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
-      ),
-    );
+            Container(
+              width: 100,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: chartData.map((data) {
+                  int index = chartData.indexOf(data) % categoryColors.length;
+                  return Row(
+                    children: [
+                      Container(
+                        width: 10,
+                        height: 10,
+                        margin: EdgeInsets.only(right: 5),
+                        color: categoryColors[index],
+                      ),
+                      Text(data.category),
+                    ],
+                  );
+                }).toList(),
+              ),
+            ),
+          ],
+        ),
+      );
+    }
   }
 }
