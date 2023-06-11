1e11bf9a95951590326ab16188cf7d87c4997564
mahakchaich <mahakchaich105@gmail.com>
2023-06-11 02:14:24 +0100

    test

--- lib/widget/pie.dart
+++ lib/widget/pie.dart
@@ -9,8 +9,9 @@ import '../Services/globals.dart';
 class ChartData {
   final String category;
   final double value;
+  final Color color;
 
-  ChartData(this.category, this.value);
+  ChartData(this.category, this.value, this.color);
 }
 
 class Pie extends StatefulWidget {
@@ -22,6 +23,11 @@ class Pie extends StatefulWidget {
 
 class _PieState extends State<Pie> {
   List<ChartData> chartData = [];
+  List<Color> categoryColors = [
+    Color.fromRGBO(255, 217, 184, 1.0),
+    Color(0xFFB2CCFF),
+    Color(0xFFFFB2D9),
+  ];
 
   @override
   void initState() {
@@ -46,7 +52,9 @@ class _PieState extends State<Pie> {
         List<ChartData> dataPoints = [];
 
         data.forEach((category, value) {
-          dataPoints.add(ChartData(category, value.toDouble()));
+          int index = dataPoints.length % categoryColors.length;
+          Color color = categoryColors[index];
+          dataPoints.add(ChartData(category, value.toDouble(), color));
         });
 
         setState(() {
@@ -63,14 +71,45 @@ class _PieState extends State<Pie> {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: SfCircularChart(
-        series: <CircularSeries>[
-          PieSeries<ChartData, String>(
-            dataSource: chartData,
-            xValueMapper: (ChartData data, _) => data.category,
-            yValueMapper: (ChartData data, _) => data.value,
-            dataLabelSettings: DataLabelSettings(
-              isVisible: true,
+      child: Row(
+        children: [
+          Expanded(
+            child: SfCircularChart(
+              series: <CircularSeries>[
+                PieSeries<ChartData, String>(
+                  dataSource: chartData,
+                  xValueMapper: (ChartData data, _) => data.category,
+                  yValueMapper: (ChartData data, _) => data.value,
+                  dataLabelSettings: DataLabelSettings(
+                    isVisible: true,
+                  ),
+                  pointColorMapper: (ChartData data, _) {
+                    int index = chartData.indexOf(data) % categoryColors.length;
+                    return categoryColors[index];
+                  },
+                ),
+              ],
+            ),
+          ),
+          Container(
+            width: 100,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: chartData.map((data) {
+                int index = chartData.indexOf(data) % categoryColors.length;
+                return Row(
+                  children: [
+                    Container(
+                      width: 10,
+                      height: 10,
+                      margin: EdgeInsets.only(right: 5),
+                      color: categoryColors[index],
+                    ),
+                    Text(data.category),
+                  ],
+                );
+              }).toList(),
             ),
           ),
         ],
